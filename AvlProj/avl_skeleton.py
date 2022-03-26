effc62265d74abcca4a0e3499520d076cb5d74b9
noac7 <nc7@mail.tau.ac.il>
2022-03-26 14:10:02 +0300

    + ids and names

--- AvlProj/avl_skeleton.py
+++ AvlProj/avl_skeleton.py
@@ -1,8 +1,8 @@
-#username - complete info
-#id1      - complete info 
-#name1    - complete info 
-#id2      - complete info
-#name2    - complete info  
+#username - nc7
+#id1      - 209223114
+#name1    - noa cohen
+#id2      - 205882236
+#name2    - eyal cohen
 
 
 
